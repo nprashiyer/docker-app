27e2d64007169e4fd082ad1eb521cd074a318871
Prashant Iyer <50290760+nprashiyer@users.noreply.github.com>
2019-10-13 15:12:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 
